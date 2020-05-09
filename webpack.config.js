a8083d80aaabb0b21c166c559bda92a67ca6b0a0
k-wasilewski <k.k.wasilewski@gmail.com>
2020-05-09 14:57:54 +0200

    added babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
         filename: "out.js",
         path: path.resolve(__dirname, "build")
     },
-    watch: true
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015']
+                }
+            }
+        }]
+    }
 }
